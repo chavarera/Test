1b4c59a23c85768710fe95ba05a74624d5cda5b9
chavarera <chavare.ravi123@gmail.com>
2018-02-02 09:14:01 -0800

    the new file added named as HelloGithub

---
+++ app/src/main/java/com/protutr/test/HelloGithub.java
@@ -0,0 +1,8 @@
+package com.protutr.test;
+
+/**
+ * Created by ravi on 2/2/2018.
+ */
+
+public class HelloGithub {
+}
--- app/src/main/java/com/protutr/test/MainActivity.java
+++ app/src/main/java/com/protutr/test/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "hello github", Toast.LENGTH_SHORT).show();
     }
 }
