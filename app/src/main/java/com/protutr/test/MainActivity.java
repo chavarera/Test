61c75289b764c42c57ecb6e5d9bf74cb2813758c
chavarera <chavare.ravi123@gmail.com>
2018-02-02 09:11:21 -0800

    Initial commit

---
+++ app/src/main/java/com/protutr/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.protutr.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+    }
+}
